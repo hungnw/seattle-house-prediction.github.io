eeba3a5fb0210e773c8f3b0cbafa49198e9fe047
Hung Nguyen <hungnw@gmail.com>
2020-08-26 18:17:02 -0700

    Update script.js

--- Model/script.js
+++ Model/script.js
@@ -15,19 +15,19 @@ function predict() {
 
     document.getElementById("predict").innerHTML = 
 
-    bed*(	-56024.756064)
-    +bath*	60835.739893
-    +living*	275.254481
-    +lot*	(-0.340061)
-    +floor	*59586.504682
-    +waterfront	*509080.734050
-    +view	*59717.481040
-    +condition*	16257.950428
+    bed *(-56024.756064)
+    +bath * 60835.739893
+    +living * 275.254481
+    +lot * (-0.340061)
+    +floor *59586.504682
+    +waterfront	* 509080.734050
+    +view * 59717.481040
+    +condition *	16257.950428
     +year_built	*(-2846.279451)
     +year_renovated	*24503.905173
     +zipcode	*(-21.497227)
     +year_sold*	27150.898226
     +month_sold*	585.632082
-
+    -47107029.94677467
 ;
 }
